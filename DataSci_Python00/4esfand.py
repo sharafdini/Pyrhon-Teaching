cd10e6f222a66e6fe0b58b3b48df73eab7d20952
Reza Sharafdini <52018758+sharafdini@users.noreply.github.com>
2021-05-10 16:43:03 +0430

    salam
    salam

---
+++ DataSci_Python00/4esfand.py
@@ -0,0 +1,10 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 2
+2
+>>> 12/5
+2.4
+>>> x = 2
+>>> x
+2
+>>> 
